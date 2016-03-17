5da7a6a72729ca293b8629c24cc666b2ae12cbfb
Edgar Sánchez <edgar.yachakuq@hotmail.com>
2016-03-17 18:01:48 -0500

    First version

---
+++ EcuadorEarthquakes/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+open System.IO
+open FSharp.Data
+
+type Sismos = HtmlProvider<"http://eventos.igepn.edu.ec/eqevents/events2.html">
+
+let headersWriter (tw:TextWriter) (headers:string[] option) =
+    match headers with
+    | Some(hs) -> tw.Write(String.Join(",", hs))
+    | _ -> tw.Write ""
+
+// Longitude and latitude come as "23° N", "1.15° S", "75° W", "34.12° E"
+// These must become "23", "-1.15", "-75", "34.12" 
+let coordToFloat posSelector (coord : string) =
+    let absValue = coord.Substring(0, coord.Length - 3)
+    if coord.EndsWith posSelector then absValue else "-" + absValue
+
+let longToFloat = coordToFloat "E" 
+let latToFloat = coordToFloat "N" 
+
+let sismWriter (tw:TextWriter) (sism:Sismos.Table1.Row) =
+    tw.Write("{0:s},{1:s},{2},{3},{4},{5},{6},{7},{8},{9:s}", 
+            sism.``Origin Time UTC``, 
+            sism.``Local Time``, 
+            sism.Mag, 
+            sism.Type, 
+            longToFloat sism.Longitude, 
+            latToFloat sism.Latitude,
+            sism.``Depth (km)``,
+            sism.``Region Name``,
+            sism.Status,
+            sism.``Last Update``)
+
+[<EntryPoint>]
+let main argv = 
+    Sismos.GetSample().Tables.Table1.Headers |> printfn "%a" headersWriter
+    let pageIndexes = "" :: ([1 .. 49] |> List.map Convert.ToString) // the pages are "", "1", "2", ..., "49" 
+    for i in pageIndexes do
+        let sismos = Sismos.Load("http://eventos.igepn.edu.ec/eqevents/events" + i + ".html")
+        for row in sismos.Tables.Table1.Rows do
+            row |> printfn "%a" sismWriter
+    0
---
+++ EcuadorEarthquakes/SeismsExploration.fsx
@@ -0,0 +1,39 @@
+﻿#r "../packages/FSharp.Data.2.2.5/lib/net40/FSharp.Data.dll"
+
+open System
+open FSharp.Data
+
+type Seisms = HtmlProvider<"http://eventos.igepn.edu.ec/eqevents/events2.html">
+
+// Longitude and latitude come as "23° N", "1.15° S", "75° W", "34.12° E"
+// These must become "23", "-1.15", "-75", "34.12" 
+let coordToFloat posSelector (coord : string) =
+    let absValue = coord.Substring(0, coord.Length - 3)
+    if coord.EndsWith posSelector then absValue else "-" + absValue
+
+let longToFloat = coordToFloat "E" 
+let latToFloat = coordToFloat "N" 
+
+let seismToString (seism: Seisms.Table1.Row) =
+    String.Format("{0:s},{1:s},{2},{3},{4},{5},{6},{7},{8},{9:s}", 
+            seism.``Origin Time UTC``, 
+            seism.``Local Time``, 
+            seism.Mag, 
+            seism.Type, 
+            longToFloat seism.Longitude, 
+            latToFloat seism.Latitude,
+            seism.``Depth (km)``,
+            seism.``Region Name``,
+            seism.Status,
+            seism.``Last Update``)
+
+let headers = String.Join (",", Seisms.GetSample().Tables.Table1.Headers.Value) 
+
+
+let pageIndexes = "" :: ([1 .. 49] |> List.map Convert.ToString) // the page indexes are "", "1", "2", ..., "49"
+
+
+let seisms = pageIndexes |> List.collect 
+                (fun pageIndex ->
+                    let page = Seisms.Load("http://eventos.igepn.edu.ec/eqevents/events" + pageIndex + ".html")
+                    page.Tables.Table1.Rows |> Array.map seismToString |> Array.toList)
